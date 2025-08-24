0a0d8ebcfff2f44e83e43c07330f758d2d84ad1a
berzz26 <aumtamboli2233@gmail.com>
2025-08-24 13:02:36 +0000

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import { Server } from 'socket.io';
+
+const app = express();
+
+const server = app.listen(8080, () => {
+  console.log(new Date(), 'server listening on 8080');
+});
+
+app.get('/', (req, res) => {
+  res.send('hey world');
+});
+
+const io = new Server(server);
+
+let users = 0;
+
+io.on('connection', (socket) => {
+  console.log(`user connected ${++users}`);
+
+  // send message on connection
+  socket.emit('message', 'Hello from Socket.IO server!!');
+
+  // listen for custom event
+  socket.on('message', (data) => {
+    console.log('received:', data);
+
+    // echo back to client
+    socket.emit('message', `server got: ${data}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`user disconnected ${--users}`);
+  });
+});
