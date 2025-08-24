fc03fb85d3ac5c285be2c6da6f14f848b8af889e
berzz26 <aumtamboli2233@gmail.com>
2025-08-24 13:12:45 +0000

    feat: add post route to create user, format options using prettier

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,18 @@
 export type WsIncoming =
-    | { type: 'join_room'; roomId: string; userId: string }
-    | { type: 'send_message'; roomId: string; userId: string; text: string }
-    | { type: 'typing'; roomId: string; userId: string; isTyping: boolean };
+  | { type: "join_room"; roomId: string; userId: string }
+  | { type: "send_message"; roomId: string; userId: string; text: string }
+  | { type: "typing"; roomId: string; userId: string; isTyping: boolean };
 
 export type WsOutgoing =
-    | { type: 'user_joined'; userId: string; count: number }
-    | { type: 'user_left'; userId: string; count: number }
-    | { type: 'new_message'; id: string; roomId: string; userId: string; text: string; sentAt: string }
-    | { type: 'typing'; userId: string; isTyping: boolean }
-    | { type: 'error'; message: string };
+  | { type: "user_joined"; userId: string; count: number }
+  | { type: "user_left"; userId: string; count: number }
+  | {
+      type: "new_message";
+      id: string;
+      roomId: string;
+      userId: string;
+      text: string;
+      sentAt: string;
+    }
+  | { type: "typing"; userId: string; isTyping: boolean }
+  | { type: "error"; message: string };
