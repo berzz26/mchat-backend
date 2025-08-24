65dbc807b533c6a235dd97eb888fe894df626092
Aum Tamboli <aumtamboli233@gmail.com>
2025-08-24 13:12:45 +0000

    feat: add post route to create user, format options using prettier

--- src/routes/room.route.ts
+++ src/routes/room.route.ts
@@ -1,23 +1,23 @@
 import { Router } from "express";
-import { prisma } from '../config/db.js'
-
+import { prisma } from "../config/db.js";
+import type { Request, Response } from "express";
 
 const router = Router();
 
-
-router.post('/create-user', async (req, res) => {
-    const { username } = req.body;
-    await prisma.user.create({
-        data: {
-            username
-        }
-    })
-})
-router.post('/create-room', async (req, res) => {
-
-})
-
-
-
-
-
+router.post("/create-user", async (req: Request, res: Response) => {
+  const { username } = req.body;
+  if (!username) {
+    return res
+      .status(401)
+      .json({ success: false, message: "username required" });
+  }
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+      },
+    });
+    res.status(201).json({ success: true, message: newUser });
+  } catch (error) {}
+});
+router.post("/create-room", async (req, res) => {});
